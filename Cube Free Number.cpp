968a42eacf1ffe68464555f0cacb378dd252a20b
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-25 18:34:14 +0700

    ลองลบแล้วเริ่มใหม่

--- Cube Free Number.cpp
+++ Cube Free Number.cpp
@@ -1,62 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	int number = 1, input, check, test;
-	loop:
-		printf("\n");
-	scanf("%d", &input);
-	if (input == 1)
-	{
-		printf("1");
-		goto loop;
-		return 0;
-	}
-	else
-	{
-		check = 1;
-		while (check < input)
-		{
-			no:
-				check++;
-				test = check;
-				for (int i = 2; i <= test; i++)
-				{
-					if (test % i == 0)
-					{
-						test /= i;
-						if (test % i == 0)
-						{
-							test /= i;
-							if (test % i == 0)
-							{
-								goto no;
-							}
-						}
-					}
-				}
-				number++;
-		}
-		check++;
-		test = check;
-		for (int i = 2; i <= test; i++)
-		{
-			if (test % i == 0)
-			{
-				test /= i;
-				if (test % i == 0)
-				{
-					test /= i;
-					if (test % i == 0)
-					{
-						printf("Not Cube Free");
-						goto loop;
-						return 0;
-					}
-				}
-			}
-		}
-		printf("%d", number);
-		goto loop;
-		return 0;
-	}
+	
+	return 0;
 }
