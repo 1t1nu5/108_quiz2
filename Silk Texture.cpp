11f600fc1e27f6fc58994be50e898921e7563dca
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-25 17:40:18 +0700

    สร้างตัวแปรรุะบุตำแหน่งของลาย แล้วสร้างค่าเริ่มต้น

--- Silk Texture.cpp
+++ Silk Texture.cpp
@@ -5,6 +5,14 @@ int main()
 	int command[3];
 	scanf("%d", &line);
 	scanf("%d", &code);
+	int cloth[line][70] = {} // line, column
+	for (int i = 0; i < line; i++)
+	{
+		for (int j = 0; j < 70; j+++)
+		{
+			cloth[i][j] = 'o';
+		}
+	}
 	while (code > 0)
 	{
 		scanf("%d %d %d", &command[0], &command[1], &command[2]);
