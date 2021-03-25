554b3851072e8a65978d0a68472249af9f2b1ef4
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-25 17:57:07 +0700

    ตรวจสอบหลักของตัวเลข

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,11 +1,34 @@
 #include <stdio.h>
 int main()
 {	
-	int limit;
+	int limit, digit = 1;
 	scanf("%d", &limit);
 	for (int i = 1; i <= limit)
 	{
-		
+		if (digit == 1 && i >= 10)
+		{
+			digit = 2;
+		}
+		else if (digit == 2 && i >= 100)
+		{
+			digit = 3;
+		}
+		else if (digit == 2 && i >= 1000)
+		{
+			digit = 4;
+		}
+		else if (digit == 2 && i >= 10000)
+		{
+			digit = 5;
+		}
+		else if (digit == 2 && i >= 100000)
+		{
+			digit = 6;
+		}
+		else
+		{
+			digit = 7;
+		}
 	}
 	return 0;
 }
